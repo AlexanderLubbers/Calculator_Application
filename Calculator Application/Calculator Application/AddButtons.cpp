2355eb2e242dc181c963eae5ee2fb8be8e318483
Chervix <wtooyah278@gmail.com>
2023-01-31 11:43:24 -0300

    added number pad

--- Calculator Application/Calculator Application/AddButtons.cpp
+++ Calculator Application/Calculator Application/AddButtons.cpp
@@ -13,39 +13,39 @@ void AddButtons::button(HWND hWnd)
     //first row
     HWND h_Left_Parentheses = CreateWindow(L"BUTTON", L"(", dwStyle, 15, 115, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
     HWND h_Right_Parantheses = CreateWindow(L"BUTTON", L")", dwStyle, 126.5, 115, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
-    HWND h_Left_Parenthes = CreateWindow(L"BUTTON", L"", dwStyle, 238, 115, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
-    HWND h_Left_Parentses = CreateWindow(L"BUTTON", L"", dwStyle, 349.5, 115, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
+    HWND h_Left_Parenthes = CreateWindow(L"BUTTON", L".", dwStyle, 238, 115, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
+    HWND h_Left_Parentses = CreateWindow(L"BUTTON", L"-", dwStyle, 349.5, 115, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
     HWND h_Left_Pareheses = CreateWindow(L"BUTTON", L"History", dwStyle, 461, 115, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
     HWND h_Left_Pantheses = CreateWindow(L"BUTTON", L"Clear", dwStyle, 572.5, 115, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
 
     //second row
     HWND h_Left_Pare = CreateWindow(L"BUTTON", L"", dwStyle, 15, 202.5, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
-    HWND h_Left_Parees = CreateWindow(L"BUTTON", L"", dwStyle, 126.5, 202.5, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
-    HWND h_Left_Parenthe = CreateWindow(L"BUTTON", L"", dwStyle, 238, 202.5, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
-    HWND h_Left_Parentss = CreateWindow(L"BUTTON", L"", dwStyle, 349.5, 202.5, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
+    HWND h_Left_Parees = CreateWindow(L"BUTTON", L"7", dwStyle, 126.5, 202.5, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
+    HWND h_Left_Parenthe = CreateWindow(L"BUTTON", L"8", dwStyle, 238, 202.5, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
+    HWND h_Left_Parentss = CreateWindow(L"BUTTON", L"9", dwStyle, 349.5, 202.5, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
     HWND h_Left_Parehees = CreateWindow(L"BUTTON", L"", dwStyle, 461, 202.5, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
     HWND h_Left_Panthses = CreateWindow(L"BUTTON", L"", dwStyle, 572.5, 202.2, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
 
     //third row
     HWND a = CreateWindow(L"BUTTON", L"", dwStyle, 15, 290, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
-    HWND b = CreateWindow(L"BUTTON", L"", dwStyle, 126.5, 290, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
-    HWND c = CreateWindow(L"BUTTON", L"", dwStyle, 238, 290, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
-    HWND d = CreateWindow(L"BUTTON", L"", dwStyle, 349.5, 290, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
+    HWND b = CreateWindow(L"BUTTON", L"4", dwStyle, 126.5, 290, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
+    HWND c = CreateWindow(L"BUTTON", L"5", dwStyle, 238, 290, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
+    HWND d = CreateWindow(L"BUTTON", L"6", dwStyle, 349.5, 290, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
     HWND e = CreateWindow(L"BUTTON", L"", dwStyle, 461, 290, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
     HWND f = CreateWindow(L"BUTTON", L"", dwStyle, 572.5, 290, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
 
     //fourth row
     HWND g = CreateWindow(L"BUTTON", L"", dwStyle, 15, 377.5, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
-    HWND h = CreateWindow(L"BUTTON", L"", dwStyle, 126.5, 377.5, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
-    HWND i = CreateWindow(L"BUTTON", L"", dwStyle, 238, 377.5, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
-    HWND j = CreateWindow(L"BUTTON", L"", dwStyle, 349.5, 377.5, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
+    HWND h = CreateWindow(L"BUTTON", L"1", dwStyle, 126.5, 377.5, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
+    HWND i = CreateWindow(L"BUTTON", L"2", dwStyle, 238, 377.5, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
+    HWND j = CreateWindow(L"BUTTON", L"3", dwStyle, 349.5, 377.5, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
     HWND k = CreateWindow(L"BUTTON", L"", dwStyle, 461, 377.5, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
     HWND l = CreateWindow(L"BUTTON", L"", dwStyle, 572.5, 377.5, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
 
     //fifth row
     HWND m = CreateWindow(L"BUTTON", L"", dwStyle, 15, 465, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
     HWND n = CreateWindow(L"BUTTON", L"", dwStyle, 126.5, 465, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
-    HWND o = CreateWindow(L"BUTTON", L"", dwStyle, 238, 465, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
+    HWND o = CreateWindow(L"BUTTON", L"0", dwStyle, 238, 465, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
     HWND p = CreateWindow(L"BUTTON", L"", dwStyle, 349.5, 465, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
     HWND q = CreateWindow(L"BUTTON", L"", dwStyle, 461, 465, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
     HWND r = CreateWindow(L"BUTTON", L"", dwStyle, 572.5, 465, 96.5, 72.5, hWnd, NULL, hInstance, NULL);
