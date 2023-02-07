6e391f4545adda6ca1eb70583dfede2e04ee1fff
Chervix <wtooyah278@gmail.com>
2023-02-07 16:22:31 -0500

    I am stupid. added a helper function and the gdiplus library needs to be installed

--- Calculator Application/Calculator Application/AddButtons.h
+++ Calculator Application/Calculator Application/AddButtons.h
@@ -1,7 +1,12 @@
 #pragma once
 #include "windows.h"
+#include "resource.h"
+//#include "gdiplus.h"
+#pragma comment (lib,"Gdiplus.lib")
+
 class AddButtons {
 public:
 	void button(HWND hWnd);
+	//Gdiplus::Bitmap* LoadImageFromResource(HMODULE hMod, const wchar_t* resid, const wchar_t* restype, HWND hWnd);
 };
 
