f01ecf05e3e2ed9b0d5d730eb82b0671abcece39
Chervix <wtooyah278@gmail.com>
2023-02-08 00:20:27 -0500

    added a helper function

--- Calculator Application/Calculator Application/AddButtons.cpp
+++ Calculator Application/Calculator Application/AddButtons.cpp
@@ -1,59 +1,59 @@
 #include "AddButtons.h"
 
 
-//Gdiplus::Bitmap* AddButtons::LoadImageFromResource(HMODULE hMod, const wchar_t* resid, const wchar_t* restype, HWND hWnd)
-//{
-//    IStream* pStream = nullptr;
-//    Gdiplus::Bitmap* pBmp = nullptr;
-//    HGLOBAL hGlobal = nullptr;
-//    HINSTANCE hInst = (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE);
-//
-//    HRSRC hrsrc = FindResourceW(hInst, resid, restype);     // get the handle to the resource
-//    if (hrsrc)
-//    {
-//        DWORD dwResourceSize = SizeofResource(hMod, hrsrc);
-//        if (dwResourceSize > 0)
-//        {
-//            HGLOBAL hGlobalResource = LoadResource(hMod, hrsrc); // load it
-//            if (hGlobalResource)
-//            {
-//                void* imagebytes = LockResource(hGlobalResource); // get a pointer to the file bytes
-//
-//                // copy image bytes into a real hglobal memory handle
-//                hGlobal = ::GlobalAlloc(GHND, dwResourceSize);
-//                if (hGlobal)
-//                {
-//                    void* pBuffer = ::GlobalLock(hGlobal);
-//                    if (pBuffer)
-//                    {
-//                        memcpy(pBuffer, imagebytes, dwResourceSize);
-//                        HRESULT hr = CreateStreamOnHGlobal(hGlobal, TRUE, &pStream);
-//                        if (SUCCEEDED(hr))
-//                        {
-//                            // pStream now owns the global handle and will invoke GlobalFree on release
-//                            hGlobal = nullptr;
-//                            pBmp = new Gdiplus::Bitmap(pStream);
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    }
-//
-//    if (pStream)
-//    {
-//        pStream->Release();
-//        pStream = nullptr;
-//    }
-//
-//    if (hGlobal)
-//    {
-//        GlobalFree(hGlobal);
-//        hGlobal = nullptr;
-//    }
-//
-//    return pBmp;
-//}
+Gdiplus::Bitmap* AddButtons::LoadImageFromResource(HMODULE hMod, const wchar_t* resid, const wchar_t* restype, HWND hWnd)
+{
+    IStream* pStream = nullptr;
+    Gdiplus::Bitmap* pBmp = nullptr;
+    HGLOBAL hGlobal = nullptr;
+    HINSTANCE hInst = (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE);
+
+    HRSRC hrsrc = FindResourceW(hInst, resid, restype);     // get the handle to the resource
+    if (hrsrc)
+    {
+        DWORD dwResourceSize = SizeofResource(hMod, hrsrc);
+        if (dwResourceSize > 0)
+        {
+            HGLOBAL hGlobalResource = LoadResource(hMod, hrsrc); // load it
+            if (hGlobalResource)
+            {
+                void* imagebytes = LockResource(hGlobalResource); // get a pointer to the file bytes
+
+                // copy image bytes into a real hglobal memory handle
+                hGlobal = ::GlobalAlloc(GHND, dwResourceSize);
+                if (hGlobal)
+                {
+                    void* pBuffer = ::GlobalLock(hGlobal);
+                    if (pBuffer)
+                    {
+                        memcpy(pBuffer, imagebytes, dwResourceSize);
+                        HRESULT hr = CreateStreamOnHGlobal(hGlobal, TRUE, &pStream);
+                        if (SUCCEEDED(hr))
+                        {
+                            // pStream now owns the global handle and will invoke GlobalFree on release
+                            hGlobal = nullptr;
+                            pBmp = new Gdiplus::Bitmap(pStream);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    if (pStream)
+    {
+        pStream->Release();
+        pStream = nullptr;
+    }
+
+    if (hGlobal)
+    {
+        GlobalFree(hGlobal);
+        hGlobal = nullptr;
+    }
+
+    return pBmp;
+}
 
 void AddButtons::button(HWND hWnd)
 {
--- Calculator Application/Calculator Application/AddButtons.h
+++ Calculator Application/Calculator Application/AddButtons.h
@@ -3,12 +3,12 @@
 #include "resource.h"
 #include <objidl.h>
 #include <gdiplus.h>
-using namespace Gdiplus;
+//using namespace Gdiplus;
 #pragma comment (lib,"Gdiplus.lib")
 
 class AddButtons {
 public:
 	void button(HWND hWnd);
-	//Gdiplus::Bitmap* LoadImageFromResource(HMODULE hMod, const wchar_t* resid, const wchar_t* restype, HWND hWnd);
+	Gdiplus::Bitmap* LoadImageFromResource(HMODULE hMod, const wchar_t* resid, const wchar_t* restype, HWND hWnd);
 };
 
