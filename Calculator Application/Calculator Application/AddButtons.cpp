13b28580a401fad7a01006f530a87e04e7859e10
SilentSchnitzel <122894453+SilentSchnitzel@users.noreply.github.com>
2023-03-03 19:05:57 -0500

    Fixed majority of symbols

--- Calculator Application/Calculator Application/AddButtons.cpp
+++ Calculator Application/Calculator Application/AddButtons.cpp
@@ -9,7 +9,6 @@ void AddButtons::button(HWND hWnd)
     DWORD dwStyleBMP = WS_VISIBLE | WS_CHILD | BS_BITMAP | BS_PUSHBUTTON;
     HINSTANCE hInstance = GetModuleHandle(NULL);
 
-
     //first row
     //(
     HWND h_Left_Parentheses = CreateWindow(L"BUTTON", L"(", dwStyle, 15, 115, 96.5, 72.5, hWnd, (HMENU)LEFT_PARENTHESES_PRESSED, NULL, NULL);
@@ -17,14 +16,12 @@ void AddButtons::button(HWND hWnd)
     HWND h_Right_Parantheses = CreateWindow(L"BUTTON", L")", dwStyle, 126.5, 115, 96.5, 72.5, hWnd, (HMENU)RIGHT_PARENTHESES_PRESSED, NULL, NULL);
     //.
     HWND h_Decimal_Point = CreateWindow(L"BUTTON", L".", dwStyle, 238, 115, 96.5, 72.5, hWnd, (HMENU)DECIMAL_PRESSED, NULL, NULL);
-
     //-
     HWND h_Negative_Symbol = CreateWindow(L"BUTTON", L"-", dwStyle, 349.5, 115, 96.5, 72.5, hWnd, (HMENU)NEGATIVE_PRESSED, NULL, NULL);
     //history button
     HWND h_History = CreateWindow(L"BUTTON", L"History", dwStyle, 461, 115, 96.5, 72.5, hWnd, (HMENU)HISTORY_PRESSED, NULL, NULL);
     //history clear
     HWND h_Clear = CreateWindow(L"BUTTON", L"Clear", dwStyle, 572.5, 115, 96.5, 72.5, hWnd, (HMENU)CLEAR_PRESSED, NULL, NULL);
-
     //second row
     //uploading the radical symbol onto a button
     HWND h_Sqroot = CreateWindowEx(NULL, L"BUTTON", NULL, dwStyleBMP, 15, 202.5, 96.5, 72.5, hWnd, (HMENU)SQROOT_PRESSED, NULL, NULL);
@@ -40,27 +37,24 @@ void AddButtons::button(HWND hWnd)
     //9
     HWND h_Nine = CreateWindow(L"BUTTON", L"9", dwStyle, 349.5, 202.5, 96.5, 72.5, hWnd, (HMENU)NINE_PRESSED, NULL, NULL);
     //division sign
-    HWND h_Division = CreateWindow(L"BUTTON", L"", dwStyleBMP, 461, 202.5, 96.5, 72.5, hWnd, (HMENU)DIVIDE_PRESSED, NULL, NULL);
-    HICON div = LoadIcon(hInstance, MAKEINTRESOURCE(DIVISION));
-    if (div == NULL) {
-        MessageBox(hWnd, L"Image failed to load", L"Error", 1);
-    }
-    SendMessage(h_Division, BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)div);
-    HWND h_Pi = CreateWindow(L"BUTTON", L"", dwStyleBMP, 572.5, 202.2, 96.5, 72.5, hWnd, (HMENU)PI_PRESSED, NULL, NULL);
-    HICON pi = LoadIcon(hInstance, MAKEINTRESOURCE(PI));
-    if (pi == NULL) {
-        MessageBox(hWnd, L"Image failed to load", L"Error", 1);
-    }
-    SendMessage(h_Pi, BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)pi);
+    HWND h_Division = CreateWindow(L"BUTTON", L"÷", dwStyle, 461, 202.5, 96.5, 72.5, hWnd, (HMENU)DIVIDE_PRESSED, NULL, NULL);
+    HWND h_Division_Handle = GetDlgItem(hWnd, DIVIDE_PRESSED);
+    // Create a new font with a larger size
+    HFONT hFontDiv = CreateFont(32, 10, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE, DEFAULT_CHARSET,
+        OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH, L"Arial");
 
+    // Send the WM_SETFONT message to the button control with the new font
+    SendMessage(h_Division_Handle, WM_SETFONT, (WPARAM)hFontDiv, TRUE);
+    HWND h_Pi = CreateWindow(L"BUTTON", L"pi", dwStyle, 572.5, 202.2, 96.5, 72.5, hWnd, (HMENU)PI_PRESSED, NULL, NULL);
     //third row
-    HWND h_Exponent = CreateWindow(L"BUTTON", NULL, dwStyleBMP, 15, 290, 96.5, 72.5, hWnd, (HMENU)EXPONENT_PRESSED, NULL, NULL);
-    HICON exponent = LoadIcon(hInstance, MAKEINTRESOURCE(EXPONENT));
-    if (exponent == NULL)
-    {
-        MessageBox(hWnd, L"Image failed to load", L"Error", 1);
-    }
-    SendMessage(h_Exponent, BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)exponent);
+    HWND h_Exponent = CreateWindow(L"BUTTON", L"^", dwStyle, 15, 290, 96.5, 72.5, hWnd, (HMENU)EXPONENT_PRESSED, NULL, NULL);
+    HWND h_Exponent_Handle = GetDlgItem(hWnd, EXPONENT_PRESSED);
+    // Create a new font with a larger size
+    HFONT hFontEx = CreateFont(25, 10, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE, DEFAULT_CHARSET,
+        OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH, L"Arial");
+
+    // Send the WM_SETFONT message to the button control with the new font
+    SendMessage(h_Exponent_Handle, WM_SETFONT, (WPARAM)hFontEx, TRUE);
     //4
     HWND h_Four = CreateWindow(L"BUTTON", L"4", dwStyle, 126.5, 290, 96.5, 72.5, hWnd, (HMENU)FOUR_PRESSED, NULL, NULL);
     //5
@@ -68,12 +62,8 @@ void AddButtons::button(HWND hWnd)
     //6
     HWND h_Six = CreateWindow(L"BUTTON", L"6", dwStyle, 349.5, 290, 96.5, 72.5, hWnd, (HMENU)SIX_PRESSESD, NULL, NULL);
     
-    HWND h_Mutiplication = CreateWindow(L"BUTTON", L"", dwStyleBMP, 461, 290, 96.5, 72.5, hWnd, (HMENU)MULTIPLY_PRESSED, NULL, NULL);
-    HICON mult = LoadIcon(hInstance, MAKEINTRESOURCE(MULTIPLICATION));
-    if (mult == NULL) {
-        MessageBox(hWnd, L"Image failed to load", L"Error", 1);
-    }
-    SendMessage(h_Mutiplication, BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)mult);
+    HWND h_Mutiplication = CreateWindow(L"BUTTON", L"X", dwStyleBMP, 461, 290, 96.5, 72.5, hWnd, (HMENU)MULTIPLY_PRESSED, NULL, NULL);
+    
     //Sine
     HWND h_Sin = CreateWindow(L"BUTTON", L"sin", dwStyle, 572.5, 290, 96.5, 72.5, hWnd, (HMENU)SINE_PRESSED, NULL, NULL);
 
@@ -87,12 +77,14 @@ void AddButtons::button(HWND hWnd)
     //3
     HWND h_Three = CreateWindow(L"BUTTON", L"3", dwStyle, 349.5, 377.5, 96.5, 72.5, hWnd, (HMENU)THREE_PRESSED, NULL, NULL);
 
-    HWND h_Subtraction = CreateWindow(L"BUTTON", L"", dwStyleBMP, 461, 377.5, 96.5, 72.5, hWnd, (HMENU)SUBTRACT_PRESSED, NULL, NULL);
-    HICON sub = LoadIcon(hInstance, MAKEINTRESOURCE(SUBTRACTION));
-    if (sub == NULL) {
-        MessageBox(hWnd, L"Image failed to load", L"Error", 1);
-    }
-    SendMessage(h_Subtraction, BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)sub);
+    HWND h_Subtraction = CreateWindow(L"BUTTON", L"--", dwStyle, 461, 377.5, 96.5, 72.5, hWnd, (HMENU)SUBTRACT_PRESSED, NULL, NULL);
+    HWND h_Subtraction_Handle = GetDlgItem(hWnd, SUBTRACT_PRESSED);
+    // Create a new font with a larger size
+    HFONT hFontSub = CreateFont(25, 10, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE, DEFAULT_CHARSET,
+        OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH, L"Arial");
+
+    // Send the WM_SETFONT message to the button control with the new font
+    SendMessage(h_Subtraction_Handle, WM_SETFONT, (WPARAM)hFontSub, TRUE);
     //cosine
     HWND h_Cos = CreateWindow(L"BUTTON", L"cos", dwStyle, 572.5, 377.5, 96.5, 72.5, hWnd, (HMENU)COSINE_PRESSED, NULL, NULL);
 
@@ -106,12 +98,14 @@ void AddButtons::button(HWND hWnd)
 
     HWND h_Comma = CreateWindow(L"BUTTON", L",", dwStyle, 349.5, 465, 96.5, 72.5, hWnd, (HMENU)COMMA_PRESSED, NULL, NULL);
 
-    HWND h_Addition = CreateWindow(L"BUTTON", L"", dwStyleBMP, 461, 465, 96.5, 72.5, hWnd, (HMENU)ADD_PRESSED, NULL, NULL);
-    HICON add = LoadIcon(hInstance, MAKEINTRESOURCE(ADDITION));
-    if (add == NULL) {
-        MessageBox(hWnd, L"Image failed to load", L"Error", 1);
-    }
-    SendMessage(h_Addition, BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)add);
+    HWND h_Addition = CreateWindow(L"BUTTON", L"+", dwStyle, 461, 465, 96.5, 72.5, hWnd, (HMENU)ADD_PRESSED, NULL, NULL);
+    HWND h_Addition_Handle = GetDlgItem(hWnd, ADD_PRESSED);
+    // Create a new font
+    HFONT hFontAddition = CreateFont(25, 10, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE, DEFAULT_CHARSET,
+        OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH, L"Arial");
+
+    // Send the WM_SETFONT message to the button control with the new font
+    SendMessage(h_Addition_Handle, WM_SETFONT, (WPARAM)hFontAddition, TRUE);
     //tangent
     HWND h_Tan = CreateWindow(L"BUTTON", L"tan", dwStyle, 572.5, 465, 96.5, 72.5, hWnd, (HMENU)TANGENT_PRESSED, NULL, NULL);
 
