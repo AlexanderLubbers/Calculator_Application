f941ddb564de20154a95ed70d5c473d35dad02f9
Chervix <122894453+SilentSchnitzel@users.noreply.github.com>
2023-03-13 20:57:25 -0400

    Finished constructor
    TODO: create actual calculator screen.

--- Calculator Application/Calculator Application/Calculator_Screen.cpp
+++ Calculator Application/Calculator Application/Calculator_Screen.cpp
@@ -1,8 +1,8 @@
 //these have to be included before any class that uses the windows api.
 //in fact that should be done for anything that does not use the winapi
 #include "rapidjson/document.h"
-#include "rapidjson/writer.h"
 #include "rapidjson/stringbuffer.h"
+#include "rapidjson/prettywriter.h"
 
 #include <fstream>
 
@@ -18,20 +18,42 @@ Calculator_Screen::Calculator_Screen()
 	Document document;
 	document.SetObject();
 
-	//test to see if the json is created?
-	document.AddMember("author", "Walter White", document.GetAllocator()); //this adds the key of author and its value to the json
-	
+	//Creating another document that will serve almost as a sub json.
+	//sub json for the equation history
+	Document EquationHistory;
+	EquationHistory.SetObject();
+	EquationHistory.AddMember("dummy", "foo", EquationHistory.GetAllocator());
+
+	//sub json for answer history
+	Document AnswerHistory;
+	AnswerHistory.SetObject();
+	AnswerHistory.AddMember("test", "test", AnswerHistory.GetAllocator());
+
+	document.AddMember("Current Equation", "", document.GetAllocator());
+	document.AddMember("Current Answer", "", document.GetAllocator());
+	document.AddMember("Equation History", EquationHistory, document.GetAllocator());
+	document.AddMember("Answer History", AnswerHistory, document.GetAllocator());
+
 	// Serialize the JSON document to a file
 	//hopefully this code will actually create the json file
 	StringBuffer buf;
-	Writer<StringBuffer> writer(buf);
+	PrettyWriter<StringBuffer> writer(buf);
 	//this line of code converts or serializes document into a stream
 	//a stream is simply an abstract representation of a sequence of data that can be read from or written to
 	//this stream will allow the user
 	//in other words this line of code seems to be an important step in creating the json file?
 	document.Accept(writer); 
-	ofstream json_file("data.json"); //creates an output file stream object called json_file and opens a new file called data.json for writing?
+	ofstream json_file("calculator_data.json"); //creates an output file stream object called json_file and opens a new file called data.json for writing?
 	json_file << buf.GetString(); //method is called to retrieve the serialized JSON data as a string. Finally, the serialized JSON data is written to the output file using <<
+}
+
+void Calculator_Screen::render_screen()
+{
+
+}
+
+void Calculator_Screen::update_json(string character)
+{
 }
 
 void Calculator_Screen::receiver(int msg)
--- Calculator Application/Calculator Application/Calculator_Screen.h
+++ Calculator Application/Calculator Application/Calculator_Screen.h
@@ -1,9 +1,15 @@
 #pragma once
+#include <iostream>
+
+using namespace std;
+
 class Calculator_Screen
 {
 public:
 	//a constructor is a function that gets called when a class is initialized
 	Calculator_Screen();
+	void render_screen();
+	void update_json(string character);
 	void receiver(int msg);
 };
 
