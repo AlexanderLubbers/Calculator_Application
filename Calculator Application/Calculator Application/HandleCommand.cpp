c2d651eef4fcbacd1e17b6f0e5930b7fe9571750
Chervix <122894453+SilentSchnitzel@users.noreply.github.com>
2023-03-23 22:45:49 -0400

    made buttons responsive
    the buttons now print their indicated symbols to the screen. only buttons that do not work are the clear, enter, and history button

--- Calculator Application/Calculator Application/HandleCommand.cpp
+++ Calculator Application/Calculator Application/HandleCommand.cpp
@@ -27,10 +27,12 @@ void HandleCommand::handle_command(HWND hWnd, WPARAM wParam)
     case DECIMAL_PRESSED:
         screen_message = ".";
         s.update_json(screen_message, false);
+        s.render_screen(hWnd);
         break;
     case NEGATIVE_PRESSED:
         screen_message = "-";
         s.update_json(screen_message, false);
+        s.render_screen(hWnd);
         break;
     case HISTORY_PRESSED:
         screen_message = "HISTORY";
@@ -43,114 +45,142 @@ void HandleCommand::handle_command(HWND hWnd, WPARAM wParam)
     case SQROOT_PRESSED:
         screen_message = "sqrt(";
         s.update_json(screen_message, false);
+        s.render_screen(hWnd);
         break;
     case SEVEN_PRESSED:
         screen_message = "7";
         s.update_json(screen_message, false);
+        s.render_screen(hWnd);
         break;
     case EIGHT_PRESSED:
         screen_message = "8";
         s.update_json(screen_message, false);
+        s.render_screen(hWnd);
         break;
     case NINE_PRESSED:
         screen_message = "9";
         s.update_json(screen_message, false);
+        s.render_screen(hWnd);
         break;
     case DIVIDE_PRESSED:
         screen_message = "/";
         s.update_json(screen_message, false);
+        s.render_screen(hWnd);
         break;
     case PI_PRESSED:
         screen_message = "pi";
         s.update_json(screen_message, false);
+        s.render_screen(hWnd);
         break;
     case EXPONENT_PRESSED:
         screen_message = "^";
         s.update_json(screen_message, false);
+        s.render_screen(hWnd);
         break;
     case FOUR_PRESSED:
         screen_message = "4";
         s.update_json(screen_message, false);
+        s.render_screen(hWnd);
         break;
     case FIVE_PRESSED:
         screen_message = "5";
         s.update_json(screen_message, false);
+        s.render_screen(hWnd);
         break;
     case SIX_PRESSESD:
         screen_message = "6";
         s.update_json(screen_message, false);
+        s.render_screen(hWnd);
         break;
     case MULTIPLY_PRESSED:
         screen_message = "*";
         s.update_json(screen_message, false);
+        s.render_screen(hWnd);
         break;
     case SINE_PRESSED:
         screen_message = "sin(";
         s.update_json(screen_message, false);
+        s.render_screen(hWnd);
         break;
     case E_PRESSED:
         screen_message = "e";
         s.update_json(screen_message, false);
+        s.render_screen(hWnd);
         break;
     case ONE_PRESSED:
         screen_message = "1";
         s.update_json(screen_message, false);
+        s.render_screen(hWnd);
         break;
     case TWO_PRESSED:
         screen_message = "2";
         s.update_json(screen_message, false);
+        s.render_screen(hWnd);
         break;
     case THREE_PRESSED:
         screen_message = "3";
         s.update_json(screen_message, false);
+        s.render_screen(hWnd);
         break;
     case SUBTRACT_PRESSED:
         screen_message = "--";
         s.update_json(screen_message, false);
+        s.render_screen(hWnd);
         break;
     case COSINE_PRESSED:
         screen_message = "cos(";
         s.update_json(screen_message, false);
+        s.render_screen(hWnd);
         break;
     case LOG_PRESSED:
         screen_message = "log(";
         s.update_json(screen_message, false);
+        s.render_screen(hWnd);
         break;
     case LOGBASE_PRESSED:
         screen_message = "logBASE(";
         s.update_json(screen_message, false);
+        s.render_screen(hWnd);
         break;
     case ZERO_PRESSED:
         screen_message = "0";
         s.update_json(screen_message, false);
+        s.render_screen(hWnd);
         break;
     case COMMA_PRESSED:
         screen_message = ",";
         s.update_json(screen_message, false);
+        s.render_screen(hWnd);
         break;
     case ADD_PRESSED:
         screen_message = "+";
         s.update_json(screen_message, false);
+        s.render_screen(hWnd);
         break;
     case TANGENT_PRESSED:
         screen_message = "tan(";
         s.update_json(screen_message, false);
+        s.render_screen(hWnd);
         break;
     case INVERSE_SINE_PRESSED:
         screen_message = "arc-sin(";
         s.update_json(screen_message, false);
+        s.render_screen(hWnd);
         break;
     case INVERSE_COSINE_PRESSED:
         screen_message = "arc-cos(";
         s.update_json(screen_message, false);
+        s.render_screen(hWnd);
         break;
     case INVERSE_TANGENT_PRESSED:
         screen_message = "arc-tan(";
         s.update_json(screen_message, false);
+        s.render_screen(hWnd);
         break;
     case ENTER_PRESSED:
         screen_message = "DELETE";
         s.update_json(screen_message, true);
+        s.render_screen(hWnd);
         break;
     }
 }
