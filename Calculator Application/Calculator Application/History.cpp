63a693823a0b63a9f3ab8bc2f898529cb3631676
Chervix <122894453+SilentSchnitzel@users.noreply.github.com>
2023-04-23 16:28:27 -0400

    finished the UI for the history window
    TODO: give the clear history button functionality

--- Calculator Application/Calculator Application/History.cpp
+++ Calculator Application/Calculator Application/History.cpp
@@ -1,4 +1,9 @@
+#include "rapidjson/document.h"
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/prettywriter.h"
+
 #include "History.h"
+#include "Global.h"
 #include <CommCtrl.h>
 #include <objidl.h>
 #include <gdiplus.h>
@@ -7,6 +12,8 @@ name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
 processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"") 
 using namespace Gdiplus;
 #pragma comment (lib,"Gdiplus.lib")
+
+using namespace rapidjson;
 
 int History::init_history(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
@@ -79,6 +86,7 @@ LRESULT History::WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
 		HFONT hOldFont = (HFONT)SelectObject(hdc, hFont);
 
+		writer();
 		SelectObject(hdc, hOldFont);
 		DeleteObject(hFont);
 		ReleaseDC(hwnd, hdc);
@@ -124,4 +132,122 @@ void History::create_buttons(HWND hwnd)
 	SendMessage(hButtonToolTip, TTM_SETMAXTIPWIDTH, 0, 500); // Set the maximum width of the ToolTip to 200 pixels
 	SendMessage(hButtonToolTip, TTM_SETDELAYTIME, TTDT_INITIAL, MAKELPARAM(0, 500)); // Set the initial delay time to 500 milliseconds
 	SendMessage(hButtonToolTip, TTM_SETTITLE, TTI_INFO, (LPARAM)L"Clear History"); // Set the title of the ToolTip
+}
+
+void History::writer()
+{
+	stringstream ss;
+	ifstream file("calculator_data.json");
+
+	ss << file.rdbuf();
+	string json_str = ss.str();
+	ss.clear();
+
+	Document doc;
+	doc.Parse(json_str.c_str());
+
+	//serializing the equations and answers into a string and then into their own document objects
+	Value& equations = doc["Equation History"];
+	Value& answers = doc["Answer History"];
+
+	Document equation_doc;
+	Document answer_doc;
+
+	StringBuffer equation_buffer;
+	StringBuffer answer_buffer;
+
+	PrettyWriter<StringBuffer> equation_writer(equation_buffer);
+	PrettyWriter<StringBuffer> answer_writer(answer_buffer);
+
+	equations.Accept(equation_writer);
+	answers.Accept(answer_writer);
+
+	string str_equations = equation_buffer.GetString();
+	string str_answers = answer_buffer.GetString();
+	
+	equation_doc.Parse(str_equations.c_str());
+	answer_doc.Parse(str_answers.c_str());
+	int count = 0;
+	//looping through each document and outputing each equation to the screen
+	for (const auto& member : equation_doc.GetObj())
+	{
+
+		const Value& value = member.value;
+		string equation_str = value.GetString();
+		if (value.IsString())
+		{
+			Global g;
+			LPCWSTR equation = g.convert_to_lpcwstr(equation_str);
+			
+			HWND hwnd = FindWindow(L"History Window", L"History");
+			HDC hdc = GetDC(hwnd);
+
+			// Create a new font
+			LOGFONT lf = { 0 };
+			lf.lfHeight = 25; // set font height to a certain amount of pixels
+			lf.lfWeight = FW_NORMAL;
+			lf.lfCharSet = DEFAULT_CHARSET;
+			lstrcpy(lf.lfFaceName, TEXT("Arial"));
+			HFONT hFont = CreateFontIndirect(&lf);
+
+			SelectObject(hdc, hFont);
+
+			int y = 159 + (30 * (count - 1));
+			TextOut(hdc, 10, y, equation, equation_str.length());
+
+			HFONT hOldFont = (HFONT)SelectObject(hdc, hFont);
+			SelectObject(hdc, hOldFont);
+			DeleteObject(hFont);
+			ReleaseDC(hwnd, hdc);
+		}
+		else
+		{
+			HWND hwnd = FindWindow(L"History Window", L"History");
+			MessageBox(hwnd, L"Error while printing equations to screen", L"Error", 1);
+		}
+		
+		count++;
+	}
+	for (const auto& member : answer_doc.GetObj())
+	{
+
+		const Value& value = member.value;
+		string answer_str = value.GetString();
+		if (value.IsString())
+		{
+			Global g;
+			LPCWSTR answer = g.convert_to_lpcwstr(answer_str);
+
+			HWND hwnd = FindWindow(L"History Window", L"History");
+			HDC hdc = GetDC(hwnd);
+
+			// Create a new font
+			LOGFONT lf = { 0 };
+			lf.lfHeight = 25; // set font height to a certain amount of pixels
+			lf.lfWeight = FW_NORMAL;
+			lf.lfCharSet = DEFAULT_CHARSET;
+			lstrcpy(lf.lfFaceName, TEXT("Arial"));
+			HFONT hFont = CreateFontIndirect(&lf);
+
+			SelectObject(hdc, hFont);
+			int y = 70 + (30 * (count - 1));
+			TextOut(hdc, 640, y, answer, answer_str.length());
+
+			HFONT hOldFont = (HFONT)SelectObject(hdc, hFont);
+			SelectObject(hdc, hOldFont);
+			DeleteObject(hFont);
+			ReleaseDC(hwnd, hdc);
+
+		}
+		else
+		{
+			HWND hwnd = FindWindow(L"History Window", L"History");
+			MessageBox(hwnd, L"Error while print answers to screen", L"Error", 1);
+		}
+		count++;
+	}
+}
+
+void History::clearer()
+{
 }
--- Calculator Application/Calculator Application/History.h
+++ Calculator Application/Calculator Application/History.h
@@ -2,14 +2,22 @@
 
 #include<Windows.h>
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstring>
+
 #define BUTTON_PRESSED 1
 
 #define WS_WINDOW (WS_THICKFRAME | WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU)
+using namespace std;
 class History
 {
 public:
 	int init_history(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow);
 	static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
     static void create_buttons(HWND hwnd);
+	static void writer();
+	static void clearer();
 };
 
